cb3f0bfc5caa8d45067e8b36fafa7f17bf028093
ned2z <139496415+ned2z@users.noreply.github.com>
2024-08-25 17:22:15 +0000

    swisstronik quest

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("swned","swn"){} 
+    constructor()ERC20("NERC","NERC"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
